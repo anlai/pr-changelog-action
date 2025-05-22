bf77d0ac2a83362c8b881c98881b9369f6d44675
Alan Lai <alan.n.lai@gmail.com>
2025-05-22 12:27:27 -0700

    test

--- latest-changes/src/index.js
+++ latest-changes/src/index.js
@@ -1,15 +1,24 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
-
 const main = require('./main.js');
 
-const filename = core.getInput('changelog-file');
-const verbose = core.getInput('verbose');
+const fs = require('fs');
+const path = require('path');
+
+// Get arguments starting from index 2 (skip node and script path)
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+  console.error("Usage: node index.js <filename> [--verbose]");
+  process.exit(1);
+}
+
+const filename = args[0];
+const verbose = args.includes('--verbose');
+
+if (verbose) {
+  console.log("Verbose mode is ON");
+}
 
 console.log(`Processing file: ${filename}...`);
 
-results = main.run(github.context, filename);
-
-console.log(results);
-
-core.setOutput('changelog', results);
+main.run(github.context, filename, verbose);
