115431a01f543138903f4393718bf71e5adeb90f
Alan Lai <alan.n.lai@gmail.com>
2025-05-22 12:18:25 -0700

    test

--- latest-changes/src/index.js
+++ latest-changes/src/index.js
@@ -6,6 +6,8 @@ const main = require('./main.js');
 const filename = core.getInput('changelog-file');
 const verbose = core.getInput('verbose');
 
+console.log(`Processing file: ${filename}...`);
+
 results = main.run(github.context, filename);
 
 console.log(results);
