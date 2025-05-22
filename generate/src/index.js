612918b344db2aa6fb76fa88bdc53ebf53edc0ab
Alan Lai <alan.n.lai@gmail.com>
2025-05-22 09:32:38 -0700

    test

--- generate/src/index.js
+++ generate/src/index.js
@@ -14,7 +14,7 @@ process.argv.forEach((val, index) => {
 
 console.log(path.resolve(__dirname));
 
-const changelogPath = `${process.argv[2]}/CHANGELOG.md`;
+const changelogPath = `${process.argv[2]}`;
 console.log(fs.readFileSync(changelogPath, 'UTF8').trim().split('\n'));
 
 console.log(github.context);
